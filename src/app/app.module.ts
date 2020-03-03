4e22eef2c49b3e80a63e395c1180f635f18cf6b6
markvanschoonhoven <42998513+markvanschoonhoven@users.noreply.github.com>
2020-03-03 19:50:23 +0100

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PlayerComponent } from './player/player.component';
+import { TeamComponent } from './team/team.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PlayerComponent,
+    TeamComponent
   ],
   imports: [
     BrowserModule,
